c99997c3a832460fa20fa3419f49cfe63f526da2
Zack Frank <frank.zack@gmail.com>
2018-08-15 15:10:54 -0500

    Add To address, copy over subject line with Vet'd FWD prefix, and temp body (Hello World) to compose msg

--- myapp.js
+++ myapp.js
@@ -8,9 +8,14 @@ InboxSDK.load(2, 'sdk_zfrank_c53eae5345').then(function(sdk){
       title: "Vet'd Forwarding",
       iconUrl: 'https://getvetd.com/wp-content/uploads/2018/03/VetdLogo-Favicon.png',
       onClick: function(event) {
+        var subject = event.threadRowView.getSubject();
         event.threadRowView.getThreadIDAsync().then(function(threadID){
           console.log(threadID);
-          sdk.Compose.openNewComposeView();
+          sdk.Compose.openNewComposeView().then(function(composeView){
+            composeView.insertTextIntoBodyAtCursor("Hello World");
+            composeView.setToRecipients(["frank.zack@gmail.com"]);
+            composeView.setSubject("Vet'd FWD: " + subject)
+          });
         }.bind(this));
       },
     });
