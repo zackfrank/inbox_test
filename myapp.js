a16dc612bb07fc911839f72ffe975933cd8e533b
Zack Frank <frank.zack@gmail.com>
2018-08-20 11:36:01 -0500

    Include sender info (name and email) in body of compose email

--- myapp.js
+++ myapp.js
@@ -1,48 +1,25 @@
-// load InboxSKD to manipulate Gmail
 InboxSDK.load(2, 'sdk_zfrank_c53eae5345').then(function(sdk){
 
-  // the SDK has been loaded
+  // the SDK has been loaded, now do something with it!
   sdk.Lists.registerThreadRowViewHandler(function(ThreadRowView){
 
-    // a ThreadRowView has come into existence
+    // a compose view has come into existence, do something with it!
     ThreadRowView.addButton({
       title: "Vet'd Forwarding",
       iconUrl: 'https://getvetd.com/wp-content/uploads/2018/03/VetdLogo-Favicon.png',
       onClick: function(event) {
-        function openEmail() {
-          var button = document.getElementsByClassName("inboxsdk__thread_row_button")[0];
-          button.setAttribute("class", "yX xY bqe");
-          if (button.classList.contains("bqe")){
-            return new Promise();
-          }
-        }
-        openEmail().then(function() {
-          var subject = event.threadRowView.getSubject();
-          event.threadRowView.getThreadIDAsync().then(function(threadID){
-            console.log(threadID);
-
-            if (document.getElementsByClassName("ams bkG")[0]) {
-              var fwdButton = document.getElementsByClassName("ams bkG")[0]
-              console.log("IF" + fwdButton)
-              eventFire(fwdButton)
-            } else {
-              function sleep(ms) {
-                return new Promise(resolve => setTimeout(resolve, ms));
-              }
-              async function fwd() {
-                await sleep(2000);
-                var fwdButton = document.getElementsByClassName("ams bkG")[0];
-                console.log("ELSE" + fwdButton);
-                // fwdButton.fireEvent('onclick');
-                // eventFire(fwdButton);
-                var evObj = document.createEvent('Events');
-                evObj.initEvent('click', true, false);
-                fwdButton.dispatchEvent(evObj);
-              }
-              fwd();
-            }
-          }.bind(this));
-        });
+        var subject = event.threadRowView.getSubject();
+        var contacts = event.threadRowView.getContacts();
+        console.log(contacts)
+        event.threadRowView.getThreadIDAsync().then(function(threadID){
+          console.log(threadID);
+          sdk.Compose.openNewComposeView().then(function(composeView){
+            composeView.insertTextIntoBodyAtCursor("Sender Name: " + contacts[0]["name"] + "\nSender Email Address: " + contacts[0]["emailAddress"]);
+            composeView.insertTextIntoBodyAtCursor();
+            composeView.setToRecipients(["frank.zack@gmail.com"]);
+            composeView.setSubject("Vet'd FWD: " + subject)
+          });
+        }.bind(this));
       },
     });
 
