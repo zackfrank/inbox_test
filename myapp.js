8ef02a65436e28252d944b3ede267aa274ea0093
Zack Frank <frank.zack@gmail.com>
2018-08-20 14:38:30 -0500

    Email is sent with sender info and subject, modal pops up to confirm

--- myapp.js
+++ myapp.js
@@ -1,33 +1,34 @@
-InboxSDK.load(2, 'sdk_zfrank_c53eae5345').then(function(sdk){
+/* InboxSDK */
+
+InboxSDK.load(2, 'sdk_zfrank_c53eae5345').then(function(sdk) {
 
   // the SDK has been loaded, now do something with it!
-  sdk.Lists.registerThreadRowViewHandler(function(ThreadRowView){
+  sdk.Lists.registerThreadRowViewHandler(function(ThreadRowView) {
 
-    // a compose view has come into existence, do something with it!
     ThreadRowView.addButton({
       title: "Vet'd Forwarding",
       iconUrl: 'https://getvetd.com/wp-content/uploads/2018/03/VetdLogo-Favicon.png',
       onClick: function(event) {
-        sdk.Compose.openNewComposeView().then(function(composeView){
+        sdk.Compose.openNewComposeView().then(function(composeView) {
+        // a compose view has come into existence
           var subject = event.threadRowView.getSubject();
           var contacts = event.threadRowView.getContacts();
-          let insertText = function() {
-            return new Promise(function(resolve, reject) {
-              composeView.insertTextIntoBodyAtCursor("Sender Name: " + contacts[0]["name"] + "\nSender Email Address: " + contacts[0]["emailAddress"]);
-              if (composeView.getTextContent().length > 5) {
-                resolve();
-              }
-            });
-          }
+          composeView.insertTextIntoBodyAtCursor("Sender Name: " + contacts[0]["name"] + "\nSender Email Address: " + contacts[0]["emailAddress"]);
           composeView.setToRecipients(["frank.zack@gmail.com"]);
           composeView.setSubject("Vet'd FWD: " + subject);
-          insertText().then(function() {
+          setTimeout(function() {
             composeView.send(sendAndArchive = true);
-          });
+            setTimeout(function() {
+              sdk.Widgets.showModalView({
+                el: "<h2>Email has been forwarded to Vet'd</h2>"
+              });
+            }, 0);
+          }, 0);
         });
       }
 
     });
 
   });
+
 });
--- myappexample.js
+++ myappexample.js
@@ -1,7 +1,9 @@
-InboxSDK.load(2, 'YOUR_APP_ID_HERE').then(function(sdk){
+/* InboxSDK */
+
+InboxSDK.load(2, 'YOUR_APP_ID_HERE').then(function(sdk) {
 
   // the SDK has been loaded, now do something with it!
-  sdk.Compose.registerComposeViewHandler(function(composeView){
+  sdk.Compose.registerComposeViewHandler(function(composeView) {
 
     // a compose view has come into existence, do something with it!
     composeView.addButton({
